747c39d8d344c6fe21145d77b1a1d9d7f7ecca5c
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-06-11 01:56:27 +0900

    test: factory에 맞춰 test 재작성

--- test/rtsp.test.js
+++ test/rtsp.test.js
@@ -1,11 +1,18 @@
 const addon = require('bindings')('rtsp_client');
 
 test('test method returns test', () => {
-    const client = new addon.RtspClientNapi({
-        id: 'test',
-        password: 'test',
-        url: 'test'
-    });
-    const test = client.test();
+    const factory = new addon.RTSPClientFactory();
+    const test = factory.test();
     expect(test).toBe('test');
 });
+
+test('play method calls callback each packet', done => {
+    const factory = new addon.RTSPClientFactory();
+    const testURL = "rtsp://localhost:554/?guid=1234567890";
+    const callback = (data) => {
+        factory.teardown();
+        expect(data).not.toBeNull();
+        done();
+    }
+    factory.play(testURL, callback);
+})
