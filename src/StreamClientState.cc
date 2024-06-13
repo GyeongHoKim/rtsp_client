83c772f46518febfb82c3d0ac9a49a8c37402952
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-06-13 22:39:56 +0900

    feat: ourRTSPClient, DummySink와 StreamClientState live555 공식 예제 참고

--- src/StreamClientState.cc
+++ src/StreamClientState.cc
@@ -1,1 +1,19 @@
 #include "StreamClientState.h"
+
+StreamClientState::StreamClientState() {
+    iter = nullptr;
+    session = nullptr;
+    subsession = nullptr;
+    streamTimerTask = nullptr;
+    duration = 0;
+}
+
+StreamClientState::~StreamClientState() {
+    delete iter;
+    if (session != nullptr) {
+        UsageEnvironment &env = session->envir();
+
+        env.taskScheduler().unscheduleDelayedTask(streamTimerTask);
+        Medium::close(session);
+    }
+}
