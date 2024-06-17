34af0d4d35c26ce7bbc27a72f793573314a6c2e3
GyeongHo Kim <rlarudgh2017@gmail.com>
2024-06-17 23:21:32 +0900

    feat: build 폴더 추가

---
+++ build/CMakeFiles/rtsp_client.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtsp_client.dir/src/GyeongHoRtspClient.cc.o"
+  "CMakeFiles/rtsp_client.dir/src/GyeongHoRtspClient.cc.o.d"
+  "CMakeFiles/rtsp_client.dir/src/addon.cc.o"
+  "CMakeFiles/rtsp_client.dir/src/addon.cc.o.d"
+  "Release/rtsp_client.node"
+  "Release/rtsp_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtsp_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
